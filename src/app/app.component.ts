c1a60d5bf30fef21517309d6ce2fa6ced9cce60b
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-03-09 22:25:30 +0300

    Begin for project
    Co-authored-by: Olcay Tapan <Olcaytp@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template:`
+  //   <h1>hello</h1>
+  // `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
