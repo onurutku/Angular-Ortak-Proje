b129a2cb5606b9e5a08fc963c11658848162dabc
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-03-09 22:27:01 +0300

    Added global styling and module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CsswithjsComponent } from './components/csswithjs/csswithjs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CsswithjsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
